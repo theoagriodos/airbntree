9ed8e14cf6047961dd925fbe68a3f39503e0bcd7
Benjamin Siret <benjaminsiret@outlook.fr>
2021-03-11 12:00:46 +0100

    add .env, and geocoder

--- app/models/tree.rb
+++ app/models/tree.rb
@@ -1,5 +1,6 @@
 class Tree < ApplicationRecord
   belongs_to :user
-
   validates :name, :description, :localisation, :price, presence: true
+  geocoded_by :localisation
+  after_validation :geocode, if: :will_save_change_to_localisation?
 end
