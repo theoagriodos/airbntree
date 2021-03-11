809413673994443ca9adc5742b7b393f513cad42
morgane duluc <morgane.duluc@gmail.com>
2021-03-11 15:04:24 +0100

    details css

--- app/models/tree.rb
+++ app/models/tree.rb
@@ -1,5 +1,5 @@
 class Tree < ApplicationRecord
   belongs_to :user
 
-  validates :name, :description, :localisation, :price, presence: true
+  validates :name, :description, :localisation, :price, :photo, presence: true
 end
