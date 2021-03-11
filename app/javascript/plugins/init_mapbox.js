20ac9536b3b1c354db86934c1636ab97cc73b934
Theo Agriodos <theo.agriodos@gmail.com>
2021-03-11 18:00:00 +0100

    map

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -5,7 +5,7 @@ const buildMap = (mapElement) => {
   mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
   return new mapboxgl.Map({
     container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v10'
+    style: 'mapbox://styles/theoagriodos/ckm53dc9xdpba17qnao286yil'
   });
 };
 
