c4a2cef536752f6a3845db53e3701e47c3120126
Theo Agriodos <theo.agriodos@gmail.com>
2021-03-12 16:49:11 +0100

    hello

--- app/models/tree.rb
+++ app/models/tree.rb
@@ -1,6 +1,7 @@
 class Tree < ApplicationRecord
   belongs_to :user
   has_one_attached :photo
+  has_many :bookings, dependent: :destroy
 
   validates :name, :description, :localisation, :price, presence: true
   geocoded_by :localisation
